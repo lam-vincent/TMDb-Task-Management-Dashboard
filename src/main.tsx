e45c3d7b68092ba4e6a475d9f33bfe35e0b8d698
Vincent <mrccniv@gmail.com>
2023-06-17 16:48:23 +0200

    Tailwind CSS setup untested

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
+import "./styles.css";
 
 import App from "./App";
 
