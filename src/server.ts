5f1d23665df706f0d0f2e9345ef004158d54dbad
Vincent <mrccniv@gmail.com>
2023-06-20 00:28:03 +0200

    Setup SQLite but not tested

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,28 @@
 import express, { Request, Response } from "express";
+import sqlite3 from "sqlite3";
+
+// Specify the path to the sqlite3.dll file
+const sqlitePath = "./lib/sqlite-dll-win64-x64-3420000/sqlite3.dll";
+
+// Create a new SQLite database connection
+const db = new sqlite3.Database(
+  ":memory:",
+  sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+  (err) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log("Connected to the SQLite database.");
+
+      // Perform database operations
+      // ...
+    }
+  }
+);
+
+// Use the SQLite database connection
+// ...
+
 const app = express();
 const port = 3000;
 
@@ -7,6 +31,8 @@ app.get("/api/tasks", (req: Request, res: Response) => {
     { id: 1, title: "Task 1", status: "inProgress" },
     { id: 2, title: "Task 2", status: "done" },
   ];
+
+  res.json(tasks);
 });
 
 app.listen(port, () => {
