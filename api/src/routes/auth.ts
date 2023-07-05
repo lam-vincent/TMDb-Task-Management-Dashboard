b1fe09f6c86942994b0e652eec508cf345ab0ac7
Vincent <mrccniv@gmail.com>
2023-07-06 01:29:40 +0200

    user login endpoint

--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -38,4 +38,32 @@ router.post("/register", async (req: Request, res: Response) => {
   }
 });
 
+router.post("/login", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  try {
+    // Check if the user exists
+    const user = await prisma.user.findUnique({ where: { username } });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    // Verify the password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ userId: user.id }, "your-secret-key", {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ message: "Login successful", token });
+  } catch (error) {
+    console.error("Login failed:", error);
+    res.status(500).json({ message: "Login failed" });
+  }
+});
+
 export default router;
