59aec1969ac1c5ae1729ba1eae648a1919cb9cf4
Vincent <mrccniv@gmail.com>
2023-08-06 10:21:25 +0200

    update pp

--- api/src/routes/userRoutes.ts
+++ api/src/routes/userRoutes.ts
@@ -17,4 +17,40 @@ router.get("/me", async (req: Request, res: Response) => {
   }
 });
 
+router.patch("/profile/picture", async (req, res) => {
+  try {
+    const { profilePictureUrl } = req.body;
+
+    const user = await prisma.user.findUnique({
+      // @ts-ignore
+      where: { id: parseInt(req.userId) },
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    if (!profilePictureUrl)
+      return res.status(400).json({ error: "Profile picture not provided" });
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        // @ts-ignore
+        id: parseInt(req.userId),
+      },
+      data: {
+        profilePictureUrl: profilePictureUrl,
+      },
+    });
+
+    res.json({
+      message: "Profile picture updated successfully",
+      user: updatedUser,
+    });
+  } catch (error) {
+    console.error("Error updating profilePicture:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
 export default router;
