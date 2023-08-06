ffa1b1aebd6775ce49bf312aff07347e331b77eb
Vincent <mrccniv@gmail.com>
2023-08-06 11:28:18 +0200

    refacto task routes in a router

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes";
 import userRoutes from "./routes/userRoutes";
+import taskRoutes from "./routes/userRoutes";
 import { authMiddleware } from "./middlewares/authMiddleware";
 
 const prisma = new PrismaClient();
@@ -12,178 +13,11 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-interface Task {
-  id: number;
-  title: string;
-  taskListId: number;
-  userId: number;
-}
-
-interface TaskList {
-  id: number;
-  title: string;
-  tasks: Task[];
-  userId: number;
-}
-
 app.use("/api/auth", authRoutes);
 
 app.use(authMiddleware);
 app.use(userRoutes);
-
-app.get("/tasklists", async (req, res) => {
-  // @ts-ignore
-  const userID = parseInt(req.userId);
-  const taskLists = await prisma.taskList.findMany({
-    where: {
-      user: {
-        id: userID,
-      },
-    },
-    include: {
-      tasks: true,
-    },
-  });
-  res.json(taskLists);
-});
-
-app.post("/tasks", async (req, res) => {
-  const { title, taskListId } = req.body;
-
-  // @ts-ignore
-  const userId = req.userId;
-
-  // reup user id
-  // 1. pass in token in frontend for all other
-  // task related routes and go to register page if not logged in
-  // 2. refactor registering account using "connect" instead
-  // of userId: userId
-  try {
-    const task = await prisma.task.create({
-      data: {
-        title,
-        taskList: {
-          connect: { id: taskListId },
-        },
-        user: {
-          connect: { id: userId },
-        },
-      },
-      include: {
-        taskList: {
-          include: {
-            user: true,
-          },
-        },
-      },
-    });
-    res.json(task);
-  } catch (error) {
-    console.error("Error creating task:", error);
-    res.status(500).json({ error: "An error occurred" });
-  }
-});
-
-app.delete("/tasks/:taskId", async (req, res) => {
-  const taskId: number = parseInt(req.params.taskId);
-
-  try {
-    const task: Task | null = await prisma.task.findUnique({
-      where: {
-        id: taskId,
-      },
-      include: {
-        taskList: {
-          include: {
-            user: true,
-          },
-        },
-      },
-    });
-
-    console.log("taskId", taskId);
-
-    if (!task) {
-      return res.status(404).json({ error: "Task not found" });
-    }
-
-    await prisma.task.delete({
-      where: {
-        id: taskId,
-      },
-    });
-
-    res.json({ message: "Task deleted successfully" });
-  } catch (error: any) {
-    console.error("Error deleting task:", error);
-    res.status(500).json({ error: "An error occurred" });
-  }
-});
-
-app.patch("/tasks/:taskId/updateTitle", async (req, res) => {
-  const taskId = parseInt(req.params.taskId);
-  const { title } = req.body;
-
-  try {
-    const task = await prisma.task.findUnique({
-      where: {
-        id: taskId,
-      },
-    });
-
-    if (!task) {
-      return res.status(404).json({ error: "Task not found" });
-    }
-
-    const updatedTask = await prisma.task.update({
-      where: {
-        id: taskId,
-      },
-      data: {
-        title: title || task.title, // Update the title if provided, otherwise keep the existing title
-      },
-    });
-
-    res.json({ message: "Task title updated successfully", task: updatedTask });
-  } catch (error) {
-    console.error("Error updating task title:", error);
-    res.status(500).json({ error: "An error occurred" });
-  }
-});
-
-app.patch("/tasks/:taskId/updateTaskListId", async (req, res) => {
-  const taskId = parseInt(req.params.taskId);
-  const { taskListId } = req.body;
-
-  try {
-    const task = await prisma.task.findUnique({
-      where: {
-        id: taskId,
-      },
-    });
-
-    if (!task) {
-      return res.status(404).json({ error: "Task not found" });
-    }
-
-    const updatedTask = await prisma.task.update({
-      where: {
-        id: taskId,
-      },
-      data: {
-        taskListId: taskListId || task.taskListId, // Update the taskListId if provided, otherwise keep the existing taskListId
-      },
-    });
-
-    res.json({
-      message: "Task taskListId updated successfully",
-      task: updatedTask,
-    });
-  } catch (error) {
-    console.error("Error updating task taskListId:", error);
-    res.status(500).json({ error: "An error occurred" });
-  }
-});
+app.use(taskRoutes);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
---
+++ api/src/routes/taskRoutes.ts
@@ -0,0 +1,175 @@
+import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const router = express.Router();
+
+interface Task {
+  id: number;
+  title: string;
+  taskListId: number;
+  userId: number;
+}
+
+interface TaskList {
+  id: number;
+  title: string;
+  tasks: Task[];
+  userId: number;
+}
+
+router.get("/tasklists", async (req, res) => {
+  // @ts-ignore
+  const userID = parseInt(req.userId);
+  const taskLists = await prisma.taskList.findMany({
+    where: {
+      user: {
+        id: userID,
+      },
+    },
+    include: {
+      tasks: true,
+    },
+  });
+  res.json(taskLists);
+});
+
+router.post("/tasks", async (req, res) => {
+  const { title, taskListId } = req.body;
+
+  // @ts-ignore
+  const userId = req.userId;
+
+  // reup user id
+  // 1. pass in token in frontend for all other
+  // task related routes and go to register page if not logged in
+  // 2. refactor registering account using "connect" instead
+  // of userId: userId
+  try {
+    const task = await prisma.task.create({
+      data: {
+        title,
+        taskList: {
+          connect: { id: taskListId },
+        },
+        user: {
+          connect: { id: userId },
+        },
+      },
+      include: {
+        taskList: {
+          include: {
+            user: true,
+          },
+        },
+      },
+    });
+    res.json(task);
+  } catch (error) {
+    console.error("Error creating task:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+router.delete("/tasks/:taskId", async (req, res) => {
+  const taskId: number = parseInt(req.params.taskId);
+
+  try {
+    const task: Task | null = await prisma.task.findUnique({
+      where: {
+        id: taskId,
+      },
+      include: {
+        taskList: {
+          include: {
+            user: true,
+          },
+        },
+      },
+    });
+
+    console.log("taskId", taskId);
+
+    if (!task) {
+      return res.status(404).json({ error: "Task not found" });
+    }
+
+    await prisma.task.delete({
+      where: {
+        id: taskId,
+      },
+    });
+
+    res.json({ message: "Task deleted successfully" });
+  } catch (error: any) {
+    console.error("Error deleting task:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+router.patch("/tasks/:taskId/updateTitle", async (req, res) => {
+  const taskId = parseInt(req.params.taskId);
+  const { title } = req.body;
+
+  try {
+    const task = await prisma.task.findUnique({
+      where: {
+        id: taskId,
+      },
+    });
+
+    if (!task) {
+      return res.status(404).json({ error: "Task not found" });
+    }
+
+    const updatedTask = await prisma.task.update({
+      where: {
+        id: taskId,
+      },
+      data: {
+        title: title || task.title, // Update the title if provided, otherwise keep the existing title
+      },
+    });
+
+    res.json({ message: "Task title updated successfully", task: updatedTask });
+  } catch (error) {
+    console.error("Error updating task title:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+router.patch("/tasks/:taskId/updateTaskListId", async (req, res) => {
+  const taskId = parseInt(req.params.taskId);
+  const { taskListId } = req.body;
+
+  try {
+    const task = await prisma.task.findUnique({
+      where: {
+        id: taskId,
+      },
+    });
+
+    if (!task) {
+      return res.status(404).json({ error: "Task not found" });
+    }
+
+    const updatedTask = await prisma.task.update({
+      where: {
+        id: taskId,
+      },
+      data: {
+        taskListId: taskListId || task.taskListId, // Update the taskListId if provided, otherwise keep the existing taskListId
+      },
+    });
+
+    res.json({
+      message: "Task taskListId updated successfully",
+      task: updatedTask,
+    });
+  } catch (error) {
+    console.error("Error updating task taskListId:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+export default router;
