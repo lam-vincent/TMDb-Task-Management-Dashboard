91543aa4e7538c2559fb8e6dbb8742faedabf430
Vincent <mrccniv@gmail.com>
2023-08-07 16:11:04 +0200

    patch("/tasklists/:taskListId/updateTaskOrder"

--- api/src/routes/tasklistRoutes.ts
+++ api/src/routes/tasklistRoutes.ts
@@ -92,4 +92,57 @@ router.patch("/tasklists/:taskListId", async (req, res) => {
   }
 });
 
+router.patch("/tasklists/:taskListId/updateTaskOrder", async (req, res) => {
+  const taskListId = parseInt(req.params.taskListId);
+  const { taskOrder } = req.body;
+
+  try {
+    const taskList = await prisma.taskList.findUnique({
+      where: {
+        id: taskListId,
+      },
+      include: {
+        tasks: true,
+      },
+    });
+
+    if (!taskList) {
+      return res.status(404).json({ error: "Task list not found" });
+    }
+
+    // Create a map of taskId to its corresponding task object
+    const taskMap: { [key: number]: Task } = {};
+    taskList.tasks.forEach((task) => {
+      taskMap[task.id] = task;
+    });
+
+    // Create a new array of tasks based on the provided task order
+    const updatedTasks: Task[] = [];
+    taskOrder.forEach((taskId: number) => {
+      const task = taskMap[taskId];
+      if (task) {
+        updatedTasks.push(task);
+      }
+    });
+
+    // Update the order of tasks in the task list
+    await prisma.taskList.update({
+      where: {
+        id: taskListId,
+      },
+      data: {
+        tasks: {
+          // Set the tasks array to the updatedTasks array
+          set: updatedTasks,
+        },
+      },
+    });
+
+    res.json({ message: "Task order updated successfully" });
+  } catch (error) {
+    console.error("Error updating task order:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
 export default router;
